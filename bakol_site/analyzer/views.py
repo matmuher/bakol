36c63cd2fb89382cd0c01f9739ed5a2e8abc3931
Matveich <matmuh316@gmail.com>
2026-04-06 07:28:38 +0300

    Crop track to 30 seconds AND add conveyer movement of chords

--- bakol_site/analyzer/views.py
+++ bakol_site/analyzer/views.py
@@ -1,32 +1,49 @@
 import os
+import sys
+import librosa
+import soundfile as sf
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings # <--- ADD THIS IMPORT
 
-import sys
+# Path fix for core
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from core.engine import Bakol
 from core.listener import Listener
 
-# Initialize core once
 bakol = Bakol()
 listener = Listener(bakol)
 
 def index(request):
     context = {}
     if request.method == 'POST' and request.FILES.get('track'):
-        # 1. Save the file
         track = request.FILES['track']
         fs = FileSystemStorage()
-        filename = fs.save(track.name, track)
-        file_url = fs.url(filename)
-        file_path = fs.path(filename)
+        
+        # 1. Save temp file
+        temp_name = fs.save("temp_" + track.name, track)
+        temp_path = fs.path(temp_name)
 
-        # 2. Call the Core
-        # We limit to 60s for the web demo to avoid timeouts
-        chords = listener.analyze_file(file_path, max_seconds=60)
+        try:
+            # 2. Crop Audio
+            max_sec = 30
+            y, sr = librosa.load(temp_path, duration=max_sec)
+            
+            cropped_filename = "cropped_" + track.name
+            # settings.MEDIA_ROOT now works because of the import above
+            cropped_path = os.path.join(settings.MEDIA_ROOT, cropped_filename)
+            
+            sf.write(cropped_path, y, sr)
 
-        # 3. Prepare data for template
-        context['chords'] = chords
-        context['file_url'] = file_url
+            # 3. Analyze
+            chords = listener.analyze_file(cropped_path, max_seconds=max_sec)
+
+            context['chords'] = chords
+            context['file_url'] = fs.url(cropped_filename)
+        finally:
+            # Cleanup
+            if os.path.exists(temp_path):
+                os.remove(temp_path)
 
     return render(request, 'index.html', context)
